9139273898bd673c7875c3e9079fce3cac62567a
Dev Iskalo <deviskalo2000@gmail.com>
2024-05-23 19:17:11 +0000

    Add input component and its story

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/utils";
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentProps, forwardRef } from "react";
+
+const inputStyles = cva([
+  "w-full",
+  "p-3",
+  "border-gray-200",
+  "rounded-lg",
+  "outline-none",
+  "placeholder:text-sm",
+  "placeholder: text-gray-400",
+  "focus:border-transparent",
+  "transition-all",
+  "duration-100",
+  "hover:border-primary-300",
+  "focus:outline-primary-400",
+  "border",
+]);
+
+type InputProps = ComponentProps<"input"> & VariantProps<typeof inputStyles>;
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type="text"
+        autoComplete="off"
+        className={cn(inputStyles(), className)}
+        {...props}
+      />
+    );
+  }
+);
